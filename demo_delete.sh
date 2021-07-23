30a8881f119e21d61704735c7fa63a6bf887b6a1
Dan Zaratsian <dzaratsian@google.com>
2021-07-23 19:05:49 +0000

    delete duplicates after moving

--- demo_delete.sh
+++
@@ -1,2 +0,0 @@
-kubectl delete namespace open-match-demo
-
--- open_match_delete.sh
+++
@@ -1,8 +0,0 @@
-
-# Delete Open Match and the corresponding sample components from this cluster
-kubectl delete psp,clusterrole,clusterrolebinding --selector=release=open-match
-kubectl delete namespace open-match
-
-# Delete GKE Cluster
-gcloud container clusters delete --zone us-west1-a open-match-cluster
-
