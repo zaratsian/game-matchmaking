bd85cea184d8336fe812a854a2bedf8966f8d968
Dan Zaratsian <dtz001@gmail.com>
2021-08-25 11:05:10 +0000

    initial build for custom pooling logic

---
+++ matchmaker103/06-view-logs-director.sh
@@ -0,0 +1,8 @@
+
+kubectl get pods -n mm102-tutorial
+
+sleep 5
+
+kubectl logs -n mm102-tutorial pod/mm102-tutorial-director
+
+
---
+++ matchmaker103/06-view-logs-frontend.sh
@@ -0,0 +1,7 @@
+
+kubectl get pods -n mm102-tutorial
+
+sleep 5
+
+kubectl logs -n mm102-tutorial pod/mm102-tutorial-frontend
+
